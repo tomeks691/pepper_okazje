dcf95bf8d0531c521c34855f4c15fff8a08d195f
tomek <luci6969@protonmail.ch>
2022-02-02 11:04:35 +0100

    Adding little improvment

--- pepper_occasions.py
+++ pepper_occasions.py
@@ -1,35 +1,54 @@
-import json
-from requests_html import HTMLSession
-from bs4 import BeautifulSoup
+import os
+import time
+from os.path import exists
+
 import telepot
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv, find_dotenv
+from requests_html import HTMLSession
+
 
 def send_deals(link):
-    with open("api_credentials.json") as f:
-        api_credentials = json.load(f)
-    api_bot = api_credentials["api"]
-    chat_id = api_credentials["chat_id"]
+    load_dotenv(find_dotenv())
+    api_bot = os.environ.get("api")
+    chat_id = os.environ.get("chat_id")
     bot = telepot.Bot(api_bot)
     bot.sendMessage(chat_id, link)
 
 
 def check_if_link_exist(link):
     link_exist = False
+    if not exists("links.txt"):
+        f = open("links.txt", "x")
+        f.close()
+
     with open("links.txt", "r") as f:
         links = f.read()
         if link not in links:
             send_deals(link)
         else:
             link_exist = True
-    if link_exist == False:
+    if not link_exist:
         with open("links.txt", "a") as f:
-            f.write(link)
-
-session = HTMLSession()
-r = session.get("https://www.pepper.pl/kupony/store.sonyentertainmentnetwork.com")
-soup = BeautifulSoup(r.content, 'html.parser')
-link = soup.select(".thread-title > a")[0].get("href")
-title = soup.select(".thread-title > a")[0].get("title")
-check_if_link_exist(title)
-check_if_link_exist(link)
+            f.write(link + "\n")
 
 
+def get_sale(group_url):
+    session = HTMLSession()
+    r = session.get(group_url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    link = soup.select(".thread-title > a")[0].get("href")
+    check_if_link_exist(link)
+
+
+if not exists("pepper_groups_to_check.txt"):
+    print("First create file pepper_groups_to_check.txt")
+    exit()
+
+with open('pepper_groups_to_check.txt') as f:
+    group_urls = f.read().splitlines()
+    if len(group_urls) == 0:
+        print("Add some links to pepper_groups_to_check.txt")
+for group_url in group_urls:
+    get_sale(group_url)
+    time.sleep(2)
